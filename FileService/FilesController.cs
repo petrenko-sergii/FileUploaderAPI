dc4641106eda98bbaed0991d967c98ff36fc9197
Petrenko Sergii <petrenko7@yahoo.com>
2025-07-08 16:02:43 +0300

    Add FilesController, Swagger for FileService-project

---
+++ FileService/FilesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileService;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<string> Get()
+    {
+        return "String from \"FileService\"";
+    }
+}
